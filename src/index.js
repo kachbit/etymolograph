fe71d3d4103a4b587fa0a3765b66c6d4bae5a104
rocketblue <59241904+kachbit@users.noreply.github.com>
2022-12-26 21:39:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-  console.log(`helloworld:   on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
